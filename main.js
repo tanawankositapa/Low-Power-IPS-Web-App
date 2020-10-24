4859eb2cf2d51328c69ffc328284efe508adaf01
tanawankositapa <60010410@kmitl.ac.th>
2020-10-25 03:07:52 +0700

    can save data to database

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const port = 3000;
 
 // เรนเดอร์ css ออกมา
 var path = require('path')
-// ประกาศ object mongoose เพื่อที่จะใช้กับ mongodb
-// var mongoose = require('mongoose');
 
 // ตั้ง express view เพื่อให้มันเรียกใช้ index.ejs
 app.set("view engine","ejs");
@@ -15,46 +13,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ประกาศ object mongoose เพื่อที่จะใช้กับ mongodb
 var mongoose = require('mongoose');
-
+// ถ้าจะ save อย่างอื่นนอกจาก string จะต้องประกาศ type แบบนี้
+var Long = require('mongodb').Long;
 mongoose.connect("mongodb://127.0.0.1:27017/beacon").then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
 
 var beaconSchema = new mongoose.Schema({
     macAddress: String,
-    rssi: Int16Array
+    // rssi: Long
 });
 
-var beaconModel = mongoose.model("beacon", beaconSchema);
+var beaconModel = mongoose.model("beaconTable", beaconSchema);
 
-var test = "Work!"
+var testX = 1.0
+var testY = 1.0 
+
 
 app.get('/',(req, res)=> {
-    
-    // todoModel.find({}, function(err,todo){
-    //     if(err) console.log(err);
-    //     else{
-    //         res.render("index.ejs",{todo:todo});  //ส่งตัวแปร local ไปยัง view // pass a local variable to the view
-    //     }
-    // })
-    
 
     // render webpage และ ส่งค่า parameter
-    res.render("index.ejs",{position:test});
-    
+    // res.render("index.ejs",{positionX:testX,positionY,testY});
+    res.render('index.ejs', { position: {x: testX, y: testY } })
 
 });
 
-// app.get('/script.js',(req, res)=> {
-    
-//     // todoModel.find({}, function(err,todo){
-//     //     if(err) console.log(err);
-//     //     else{
-//     //         res.render("index.ejs",{todo:todo});  //ส่งตัวแปร local ไปยัง view // pass a local variable to the view
-//     //     }
-//     // })
-//     res.render("index.ejs")
-    
+var saveData = new beaconModel({
+    'mac': "E0:D9:DA:22:34:1B",
+    // 'time': Math.floor(Date.now() / 1000) // Time of save the data in unix timestamp format
+}).save(function(err, result) {
+    if (err) throw err;
 
-// });
-
+    if(result) {
+        console.log(result)
+    }
+});
 
 app.listen(port, () => console.log("Server running at port " + port));
